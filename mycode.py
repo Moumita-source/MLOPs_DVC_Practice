5407889c3be1a1dea8d944eeaf898267a57c68b7
Moumita <palitmoumita5@gmail.com>
2026-01-12 04:47:29 -0800

    Second version of data added

--- mycode.py
+++ mycode.py
@@ -10,6 +10,10 @@ data = { "name": ["Default User", "Alice", "Bob"],
 df = pd.DataFrame(data)
 # print(f"My data is :\n {df}")
 
+# Add a row in the dataframe
+new_row = ['Moumita', 'palitmoumita5@gamil.com', 'mou', 'False']
+df.loc[len(df.index)] = new_row
+
 
 dir = 'data'
 os.makedirs(dir, exist_ok= True)
